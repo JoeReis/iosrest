5fe1c8c468718346574b1670387a26c58d1087b6
Joe Reis <josephreis@gmail.com>
2015-02-01 11:11:26 -0700

    Example of iOS app that reads from a rest api.

--- RestExample/ViewController.h
+++ RestExample/ViewController.h
@@ -4,11 +4,18 @@
 //
 //  Created by Joseph Reis on 2/1/15.
 //  Copyright (c) 2015 JoeReis. All rights reserved.
+
+//source: https://spring.io/guides/gs/consuming-rest-ios/
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UILabel *greetingId;
+@property (nonatomic, strong) IBOutlet UILabel *greetingContent;
+
+- (IBAction)fetchGreeting;
 
 
 @end
